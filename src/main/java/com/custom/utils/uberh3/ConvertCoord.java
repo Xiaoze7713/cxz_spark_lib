e03ff307fe34a068cbbfa4ce077dafc15cf2355e
sunmingze <sunmingze@sinoiov.com>
2019-09-25 15:42:07 +0800

    add  convert

---
+++ src/main/java/com/custom/utils/uberh3/ConvertCoord.java
@@ -0,0 +1,21 @@
+package com.custom.utils.uberh3;
+
+import com.custom.utils.uberh3.GeoUtils.CoordType;
+import com.custom.utils.uberh3.GeoUtils.GeoConvertUtils;
+import com.custom.utils.uberh3.GeoUtils.Point;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class ConvertCoord extends UDF {
+    public Text evaluate(Text lng, Text lat, Text from, Text to) {
+        Point point = new Point(Double.parseDouble(lng.toString()), Double.parseDouble(lat.toString()));
+        CoordType coordFrom = CoordType.codeOf(from.toString());
+        CoordType coordTo = CoordType.codeOf(to.toString());
+        Point newPoint =  GeoConvertUtils.convertCoord(point, coordFrom, coordTo);
+        StringBuffer sb = new StringBuffer();
+        sb.append(newPoint.getLon());
+        sb.append(",");
+        sb.append(newPoint.getLat());
+        return new Text(sb.toString());
+    }
+}
---
+++ src/main/java/com/custom/utils/uberh3/CoordConvertAndGeo2H3.java
@@ -0,0 +1,26 @@
+package com.custom.utils.uberh3;
+
+import com.custom.utils.uberh3.GeoUtils.CoordType;
+import com.custom.utils.uberh3.GeoUtils.GeoConvertUtils;
+import com.custom.utils.uberh3.GeoUtils.Point;
+import com.uber.h3core.H3Core;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class CoordConvertAndGeo2H3 extends UDF {
+    static private H3Core h3;
+    public Text evaluate(Text lng, Text lat, Text deep, Text from, Text to) {
+        Point point = new Point(Double.parseDouble(lng.toString()), Double.parseDouble(lat.toString()));
+        CoordType coordFrom = CoordType.codeOf(from.toString());
+        CoordType coordTo = CoordType.codeOf(to.toString());
+        Point newPoint =  GeoConvertUtils.convertCoord(point, coordFrom, coordTo);
+        int ideep = Integer.parseInt(deep.toString());
+        Text res = new Text();
+        res.set(Long.toHexString(h3.geoToH3(newPoint.getLat(), newPoint.getLon(), ideep)));
+        return res;
+    }
+    public CoordConvertAndGeo2H3() throws Exception {
+        h3 = H3Core.newInstance();
+    }
+
+}
